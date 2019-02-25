ac900669d10dc7dec54417d0f9fb7c692bbec44f
Rezaur Rahman <rezaur.rahman@inoviagroup.se>
2019-02-25 16:15:10 +0100

    Reza

--- src/components/TranscriptionList.js
+++ src/components/TranscriptionList.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
-import moment from 'moment';
-import { EuiInMemoryTable, EuiButtonEmpty } from '@elastic/eui';
+import React, { Component } from 'react'
+import moment from 'moment'
+import { EuiInMemoryTable, EuiButtonEmpty } from '@elastic/eui'
 
 export default class TransactionList extends Component {
   static columns = [
     {
-      field: 'createdAt', name: 'Created', sortable: true, render: created => moment(created).format('YYYY-MM-DD HH:mm:ss') 
+      field: 'createdAt', name: 'Created', sortable: true, render: created => moment(created).format('YYYY-MM-DD HH:mm:ss')
     },
     {
       field: 'updatedAt', name: 'Last updated', sortable: true, render: updated => moment(updated).format('YYYY-MM-DD HH:mm:ss')
@@ -17,11 +17,11 @@ export default class TransactionList extends Component {
       name: 'File',
       sortable: true,
       render: (url, item) => {
-        const fileName = url.replace('http://localhost:9000/minio/transcriptions/', '');
-        return <EuiButtonEmpty iconType="play" href={`/#edit/${item.id}`}>{fileName}</EuiButtonEmpty>;
-      },
+        const fileName = url.replace('http://localhost:9000/minio/transcriptions/', '')
+        return <EuiButtonEmpty iconType="play" href={`/#edit/${item.id}`}>{fileName}</EuiButtonEmpty>
+      }
     },
-    { field: 'status', name: 'Status', sortable: true },
+    { field: 'status', name: 'Status', sortable: true }
   ]
 
   static randomContent = [
@@ -31,7 +31,7 @@ export default class TransactionList extends Component {
       status: 'this status',
       url: 'http://localhost:9000/minio/transcriptions/858048.wav',
       createdAt: '2019-02-06T09:04:39.041+0000',
-      updatedAt: '2019-02-06T09:04:39.041+0000',
+      updatedAt: '2019-02-06T09:04:39.041+0000'
     },
     {
       id: 2,
@@ -39,126 +39,13 @@ export default class TransactionList extends Component {
       status: 'some status',
       url: 'http://localhost:9000/minio/transcriptions/858058.wav',
       createdAt: '2019-02-06T09:05:39.041+0000',
-      updatedAt: '2019-02-06T09:05:39.041+0000',
-    },
-    {
-      id: 3,
-      text: 'some text3',
-      status: 'some status',
-      url: 'http://localhost:9000/minio/transcriptions/858058.wav',
-      createdAt: '2019-02-06T09:05:39.041+0000',
-      updatedAt: '2019-02-06T09:05:39.041+0000',
-    },
-    {
-      id: 4,
-      text: 'some text4',
-      status: 'some status',
-      url: 'http://localhost:9000/minio/transcriptions/858058.wav',
-      createdAt: '2019-02-06T09:05:39.041+0000',
-      updatedAt: '2019-02-06T09:05:39.041+0000',
-    },
-    {
-      id: 5,
-      text: 'some text5',
-      status: 'some status',
-      url: 'http://localhost:9000/minio/transcriptions/858058.wav',
-      createdAt: '2019-02-06T09:05:39.041+0000',
-      updatedAt: '2019-02-06T09:05:39.041+0000',
-    },
-    {
-      id: 6,
-      text: 'some text6',
-      status: 'some status',
-      url: 'http://localhost:9000/minio/transcriptions/858058.wav',
-      createdAt: '2019-02-06T09:05:39.041+0000',
-      updatedAt: '2019-02-06T09:05:39.041+0000',
-    },
-    {
-      id: 7,
-      text: 'some text7',
-      status: 'some status',
-      url: 'http://localhost:9000/minio/transcriptions/858058.wav',
-      createdAt: '2019-02-06T09:05:39.041+0000',
-      updatedAt: '2019-02-06T09:05:39.041+0000',
-    },
-    {
-      id: 8,
-      text: 'some text8',
-      status: 'some status',
-      url: 'http://localhost:9000/minio/transcriptions/858058.wav',
-      createdAt: '2019-02-06T09:05:39.041+0000',
-      updatedAt: '2019-02-06T09:05:39.041+0000',
-    },
-    {
-      id: 8,
-      text: 'some text8',
-      status: 'some status',
-      url: 'http://localhost:9000/minio/transcriptions/858058.wav',
-      createdAt: '2019-02-06T09:05:39.041+0000',
-      updatedAt: '2019-02-06T09:05:39.041+0000',
-    },
-    {
-      id: 9,
-      text: 'some text9',
-      status: 'some status',
-      url: 'http://localhost:9000/minio/transcriptions/858058.wav',
-      createdAt: '2019-02-06T09:05:39.041+0000',
-      updatedAt: '2019-02-06T09:05:39.041+0000',
-    },
-    {
-      id: 10,
-      text: 'some text10',
-      status: 'some status',
-      url: 'http://localhost:9000/minio/transcriptions/858058.wav',
-      createdAt: '2019-02-06T09:05:39.041+0000',
-      updatedAt: '2019-02-06T09:05:39.041+0000',
-    },
-    {
-      id: 11,
-      text: 'some text11',
-      status: 'some status',
-      url: 'http://localhost:9000/minio/transcriptions/858058.wav',
-      createdAt: '2019-02-06T09:05:39.041+0000',
-      updatedAt: '2019-02-06T09:05:39.041+0000',
-    },
-
-    {
-      id: 12,
-      text: 'some text12',
-      status: 'some status',
-      url: 'http://localhost:9000/minio/transcriptions/858058.wav',
-      createdAt: '2019-02-06T09:05:39.041+0000',
-      updatedAt: '2019-02-06T09:05:39.041+0000',
-    },
-    {
-      id: 13,
-      text: 'some text13',
-      status: 'some status',
-      url: 'http://localhost:9000/minio/transcriptions/858058.wav',
-      createdAt: '2019-02-06T09:05:39.041+0000',
-      updatedAt: '2019-02-06T09:05:39.041+0000',
-    },
-    {
-      id: 14,
-      text: 'some text14',
-      status: 'some status',
-      url: 'http://localhost:9000/minio/transcriptions/858058.wav',
-      createdAt: '2019-02-06T09:05:39.041+0000',
-      updatedAt: '2019-02-06T09:05:39.041+0000',
-    },
-    {
-      id: 15,
-      text: 'some text15',
-      status: 'some status',
-      url: 'http://localhost:9000/minio/transcriptions/858058.wav',
-      createdAt: '2019-02-06T09:05:39.041+0000',
-      updatedAt: '2019-02-06T09:05:39.041+0000',
-    },
+      updatedAt: '2019-02-06T09:05:39.041+0000'
+    }
   ]
 
   render() {
-    const items = TransactionList.randomContent;
-    const { columns } = TransactionList;
+    const items = TransactionList.randomContent
+    const { columns } = TransactionList
     return (
       <EuiInMemoryTable
         pagination
@@ -166,6 +53,6 @@ export default class TransactionList extends Component {
         columns={columns}
         items={items}
       />
-    );
+    )
   }
 }
