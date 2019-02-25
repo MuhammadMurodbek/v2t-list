82a1e67c863fbe0ec012a975abd63e8aa9811a14
M Rezaur Rahman <mrezaurrahman@icloud.com>
2019-02-25 15:38:53 +0100

    Fix eslint errors

--- src/components/TranscriptionList.js
+++ src/components/TranscriptionList.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
-import moment from 'moment';
-import { EuiInMemoryTable, EuiButtonEmpty } from '@elastic/eui';
+import React, { Component } from 'react'
+import moment from 'moment'
+import { EuiInMemoryTable, EuiButtonEmpty } from '@elastic/eui'
 
 export default class TransactionList extends Component {
   static columns = [
     {
-      field: 'createdAt', name: 'Created', sortable: true, render: created => moment(created).format('YYYY-MM-DD HH:mm:ss') 
+      field: 'createdAt', name: 'Created', sortable: true, render: created => moment(created).format('YYYY-MM-DD HH:mm:ss')
     },
     {
       field: 'updatedAt', name: 'Last updated', sortable: true, render: updated => moment(updated).format('YYYY-MM-DD HH:mm:ss')
@@ -17,11 +17,11 @@ export default class TransactionList extends Component {
       name: 'File',
       sortable: true,
       render: (url, item) => {
-        const fileName = url.replace('http://localhost:9000/minio/transcriptions/', '');
-        return <EuiButtonEmpty iconType="play" href={`/#edit/${item.id}`}>{fileName}</EuiButtonEmpty>;
-      },
+        const fileName = url.replace('http://localhost:9000/minio/transcriptions/', '')
+        return <EuiButtonEmpty iconType="play" href={`/#edit/${item.id}`}>{fileName}</EuiButtonEmpty>
+      }
     },
-    { field: 'status', name: 'Status', sortable: true },
+    { field: 'status', name: 'Status', sortable: true }
   ]
 
   static randomContent = [
@@ -31,7 +31,7 @@ export default class TransactionList extends Component {
       status: 'this status',
       url: 'http://localhost:9000/minio/transcriptions/858048.wav',
       createdAt: '2019-02-06T09:04:39.041+0000',
-      updatedAt: '2019-02-06T09:04:39.041+0000',
+      updatedAt: '2019-02-06T09:04:39.041+0000'
     },
     {
       id: 2,
@@ -39,13 +39,13 @@ export default class TransactionList extends Component {
       status: 'some status',
       url: 'http://localhost:9000/minio/transcriptions/858058.wav',
       createdAt: '2019-02-06T09:05:39.041+0000',
-      updatedAt: '2019-02-06T09:05:39.041+0000',
-    },
+      updatedAt: '2019-02-06T09:05:39.041+0000'
+    }
   ]
 
   render() {
-    const items = TransactionList.randomContent;
-    const { columns } = TransactionList;
+    const items = TransactionList.randomContent
+    const { columns } = TransactionList
     return (
       <EuiInMemoryTable
         pagination
@@ -53,6 +53,6 @@ export default class TransactionList extends Component {
         columns={columns}
         items={items}
       />
-    );
+    )
   }
 }
