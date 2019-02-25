96f2a45a43ad2a2a8c2aac6290d813d8f0cb9428
M Rezaur Rahman <mrezaurrahman@icloud.com>
2019-02-25 13:18:49 +0100

    Fix eslint issue from Edit component

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,41 +1,41 @@
-import React, { Component } from 'react'
-import { EuiFlexGroup, EuiFlexItem } from '@elastic/eui'
+import React, { Component } from 'react';
+import { EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
 
-import Page from '../components/Page'
-import Editor from '../components/Editor'
+import Page from '../components/Page';
+import Editor from '../components/Editor';
 
 import Audio from '../audio/audiodesc.mp3';
 import Transcript from '../audio/audiodesc.json';
 import TranscriptSub from '../audio/audiodesc.vtt';
 
 export default class EditPage extends Component {
-
   state = {
-    transcript: null
+    transcript: null,
   }
 
   componentDidMount() {
-    this.ref = React.createRef()
-    this.updateSubtitles()
+    this.ref = React.createRef();
+    this.updateSubtitles();
   }
 
   updateSubtitles = () => {
-    const transcript = this.getSubtitles()
-    this.setState({ transcript })
+    const transcript = this.getSubtitles();
+    this.setState({ transcript });
   }
 
   getSubtitles = () => {
-    const currentTime = this.ref.current ? this.ref.current.currentTime : null
-    return Transcript.map((d, i) => {
-      if (currentTime > d.start && currentTime < d.end)
-        return <span key={i} style={{ fontWeight: 'bold', color: "#0079A5" }}>{d.text} </span>
-      else
-        return <span key={i}>{d.text} </span>
-    })
+    const currentTime = this.ref.current ? this.ref.current.currentTime : null;
+
+    return Transcript.map((d) => {
+      if (currentTime > d.start && currentTime < d.end) {
+        return <span style={{ fontWeight: 'bold', color: '#0079A5' }}>{d.text}</span>;
+      }
+      return <span>{d.text}</span>;
+    });
   }
 
   render() {
-    const { transcript } = this.state
+    const { transcript } = this.state;
     return (
       <Page title="Editor">
         <EuiFlexGroup direction="column">
@@ -46,7 +46,7 @@ export default class EditPage extends Component {
                 src={Audio}
                 ref={this.ref}
                 onTimeUpdate={this.updateSubtitles}
-                style={{width: '100%'}}
+                style={{ width: '100%' }}
               >
                 <track
                   default
@@ -65,6 +65,6 @@ export default class EditPage extends Component {
           </EuiFlexItem>
         </EuiFlexGroup>
       </Page>
-    )
+    );
   }
 }
