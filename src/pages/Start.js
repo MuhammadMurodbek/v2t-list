13e8ce65e9879a5ee1ee2c6e25f84b9c25c90777
M Rezaur Rahman <mrezaurrahman@icloud.com>
2019-02-25 12:37:32 +0100

    Fix eslint issue from Start component

--- src/pages/Start.js
+++ src/pages/Start.js
@@ -1,16 +1,12 @@
-import React, { Component } from 'react'
-import { EuiI18n } from '@elastic/eui'
+import React from 'react';
+import { EuiI18n } from '@elastic/eui';
+import Page from '../components/Page';
+import TranscriptionList from '../components/TranscriptionList';
 
-import Page from '../components/Page'
-import TranscriptionList from '../components/TranscriptionList'
+const StartPage = () => (
+  <Page title={<EuiI18n token="startTitle" default="Active Back Log" />}>
+    <TranscriptionList />
+  </Page>
+);
 
-export default class StartPage extends Component {
-
-  render() {
-    return (
-      <Page title={<EuiI18n token="startTitle" default="Active Back Log" />}>
-        <TranscriptionList />
-      </Page>
-    )
-  }
-}
+export default StartPage;
