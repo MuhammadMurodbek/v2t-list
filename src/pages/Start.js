5bbc1d45da9e619c613434b31ca5d993bb92eabf
Rezaur Rahman <rezaur.rahman@inoviagroup.se>
2019-02-25 16:11:45 +0100

    Reza

--- src/pages/Start.js
+++ src/pages/Start.js
@@ -1,16 +1,12 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { EuiI18n } from '@elastic/eui'
-
 import Page from '../components/Page'
 import TranscriptionList from '../components/TranscriptionList'
 
-export default class StartPage extends Component {
+const StartPage = () => (
+  <Page title={<EuiI18n token="startTitle" default="Active Back Log" />}>
+    <TranscriptionList />
+  </Page>
+)
 
-  render() {
-    return (
-      <Page title={<EuiI18n token="startTitle" default="Active Back Log" />}>
-        <TranscriptionList />
-      </Page>
-    )
-  }
-}
+export default StartPage
