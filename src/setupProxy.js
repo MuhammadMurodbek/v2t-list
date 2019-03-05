0a22256a906e93618bf8e6e14234159c2816a6eb
M Rezaur Rahman <mrezaurrahman@icloud.com>
2019-03-05 13:06:21 +0100

    Fix docker-compose issue for proxy setup

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,5 @@
 const proxy = require('http-proxy-middleware')
 
 module.exports = (app) => {
-  app.use(proxy('/api', { target: 'http://localhost:6102/', secure: false, cahngeOrigin: true }))
+  app.use(proxy('/api', { target: 'http://storage-service:6100/', secure: false, cahngeOrigin: true }))
 }
