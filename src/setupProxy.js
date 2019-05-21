bddd065e5e2cf501041e39956cb787a769a6e105
M Rezaur Rahman <mrezaurrahman@icloud.com>
2019-05-21 10:38:07 +0200

    Fix proxy issue for getting the id of a live transcript

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,10 +4,6 @@ module.exports = (app) => {
   app.use(proxy('/api/v1/workflow', { target: 'http://v2t-service:6100/', secure: false, cahngeOrigin: true }))
   app.use(proxy('/api/v1/transcription', { target: 'http://v2t-service:6100/', secure: false, cahngeOrigin: true }))
   app.use(proxy('/api/v1/transcription', { target: 'http://v2t-service:6100/', secure: false, cahngeOrigin: true }))
-  // app.use(proxy('/api/v1/v2t-realtime', { target: 'http://v2t-service:6100/', secure: false, cahngeOrigin: true }))
   app.use(proxy('/api/v1/code-service', { target: 'http://code-service:7004/', secure: false, cahngeOrigin: true }))
-  app.use(proxy('/api/v1/v2t-realtime', { target: 'http://realtime-service:6100/', secure: false, cahngeOrigin: true }))
-  app.use(proxy('/api/v1/v2t-service-realtime/', { target: 'http://v2t-service:6100/', secure: false, cahngeOrigin: true }))
+  app.use(proxy('/api/v1/v2t-realtime/', { target: 'http://realtime-service:6100/', secure: false, cahngeOrigin: true }))
 }
-
-
