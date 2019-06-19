1722e06b3fa7ac7b36e461ea1ce24b35b07d6aea
M Rezaur Rahman <mrezaurrahman@icloud.com>
2019-06-19 16:18:34 +0200

    WIP

--- src/pages/Training.js
+++ src/pages/Training.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import axios from 'axios'
 import {
   EuiFlexGroup,
   EuiFlexItem,
   EuiButton,
   EuiSpacer,
-  EuiTitle
+  EuiTitle,
+  EuiGlobalToastList,
+  EuiProgress
 } from '@elastic/eui'
 import Player from '../components/Player'
 import Editor from '../components/Editor'
@@ -25,12 +27,26 @@ export default class UploadPage extends Component {
           startTime: 0,
           words: 'Gott och opåverkad. Inga kardiella inkompensationstecken i vila. Pollenallergiker'
         }]
-    }]
+    }],
+    trasncriptId: null,
+    toasts: []
   }
 
   componentDidMount = async () => {
     document.title = 'Inovia AI :: Training ⛷'
     this.playerRef = React.createRef()
+    this.loadCurrentTranscript()
+  }
+
+  loadCurrentTranscript = async () => {
+    const status = await axios.get('/api/v1/training/')
+    if (Object.prototype.hasOwnProperty.call(status, 'transcription')) {
+      this.setState({
+        trasncriptId: status.transcription.transcriptionId
+      })
+    } else {
+      // alert('Yayyyy, all transcripts have feedback now')
+    }
   }
 
   onTimeUpdate = () => {
@@ -65,28 +81,78 @@ export default class UploadPage extends Component {
     }
   }
 
-  completeTranscript = () => {
-    // TODO
-    // Mark the transcript as complete
-    // Redirect to the next incomplete one
-    // If there is none, congratulate the user
+  completeTranscript = async () => {
+    // const status = await axios.get('/api/v1/training/')
+    // console.log('status')
+    // console.log(status)
+    this.setState({
+      toasts: [{
+        title: '',
+        color: 'success',
+        text: (
+          <Fragment>
+            <h3>Saving training data</h3>
+            <EuiProgress size="s" color="subdued" />
+          </Fragment>)
+      }]
+    })
   }
 
   skipTranscript = () => {
     // TODO
     // Redirect to the next incomplete one
     // If there is none, let the user know that it is the last one to be completed
+    this.setState({
+      toasts: [{
+        title: '',
+        color: 'primary',
+        text: (
+          <Fragment>
+            <h3>Loading new training data</h3>
+            <EuiProgress size="s" color="subdued" />
+          </Fragment>)
+      }]
+    })
+    this.loadCurrentTranscript()
   }
 
-  rejectTranscript = () => {
-    // TODO
-    // Redirect to the next incomplete one
-    // If there is none, congratulate the user
+  rejectTranscript = async () => {
+    // const { trasncriptId, chapters } = this.state
+    // let trainingText = ''
+    // chapters.forEach((chapter) => {
+    //   trainingText = `${trainingText} ${chapter.segment}`
+    // })
+
+    // const rejectionStatus = await axios.post(`/api/v1/training/${trasncriptId}/0/0`, {
+    //   text: trainingText,
+    //   status: 'REJECT'
+    // })
+
+    this.setState({
+      toasts: [{
+        title: '',
+        color: 'danger',
+        text: (
+          <Fragment>
+            <h3>Rejecting training data</h3>
+            <EuiProgress size="s" color="subdued" />
+          </Fragment>)
+      }]
+    })
+
+    // if (rejectionStatus) {
+    //   alert('The training data is marked as rejected.')
+    //   window.location.replace('/training')
+    // }
   }
 
 
   onValidateTranscript = (errors) => {
     this.setState({ errors })
+  }
+
+  removeToast = () => {
+    this.setState({ toasts: [] })
   }
 
 
@@ -96,7 +162,9 @@ export default class UploadPage extends Component {
       queryTerm,
       chapters,
       currentTime,
-      isBeingEdited
+      isBeingEdited,
+      trasncriptId,
+      toasts
     } = this.state
 
     return (
@@ -108,8 +176,7 @@ export default class UploadPage extends Component {
         <EuiFlexGroup alignItems="center">
           <EuiFlexItem>
             <Player
-            //   audioTranscript={originalChapters}
-              trackId="24453e3d-d473-47f5-b7be-74d2c8ba8f37"
+              trackId={trasncriptId}
               getCurrentTime={this.getCurrentTime}
               updateSeek={this.onTimeUpdate}
               queryTerm={queryTerm}
@@ -149,6 +216,11 @@ export default class UploadPage extends Component {
             <EuiButton fill color="danger" onClick={this.rejectTranscript}>Reject</EuiButton>
           </EuiFlexItem>
         </EuiFlexGroup>
+        <EuiGlobalToastList
+          toasts={toasts}
+          dismissToast={this.removeToast}
+          toastLifeTimeMs={2000}
+        />
       </Page>
     )
   }
