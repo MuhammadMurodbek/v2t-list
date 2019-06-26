efe377c2e5ddd1c8e0024cef6338b14e8d966ac1
M Rezaur Rahman <mrezaurrahman@icloud.com>
2019-06-26 14:45:16 +0200

    Add Training button and instruction

---
+++ src/components/TrainingButtonActions.js
@@ -0,0 +1,51 @@
+// Used react synthetic event
+import React, { Fragment } from 'react'
+import { EuiBasicTable, EuiSpacer, EuiIcon } from '@elastic/eui'
+import '../App.css'
+
+const TrainingInstructions = () => {
+  const columns = [
+    {
+      field: 'button',
+      name: 'Button',
+      width: '100px'
+    },
+    {
+      field: 'usage',
+      name: 'Usage'
+    }
+  ]
+
+  const items = [
+    {
+      button: 'Complete',
+      usage: 'Complete the transcript.'
+    },
+    {
+      button: 'Skip',
+      usage: 'Skip the transcript. If you use this option, this text might come up again later.'
+    },
+    {
+      button: 'Reject',
+      usage: 'Mark this transcript as a bad one.'
+    },
+    {
+      button: '⚙️',
+      usage: 'Use this button for additional configuration.'
+    }
+  ]
+
+  return (
+    <Fragment>
+        Button Reference
+      <EuiSpacer size="s" />
+      <EuiBasicTable
+        items={items}
+        columns={columns}
+        responsive
+      />
+    </Fragment>
+  )
+}
+
+export default TrainingInstructions
---
+++ src/components/TrainingInstructions.js
@@ -0,0 +1,58 @@
+// Used react synthetic event
+import React, { Fragment } from 'react'
+import { EuiBasicTable, EuiSpacer } from '@elastic/eui'
+import '../App.css'
+
+const TrainingInstructions = () => {
+  const columns = [
+    {
+      field: 'action',
+      name: 'Action',
+      width: '200px'
+    },
+    {
+      field: 'shortcut',
+      name: 'Shortcut'
+    }
+  ]
+
+  const items = [
+    {
+      action: 'Play',
+      shortcut: 'alt + space'
+    },
+    {
+      action: 'Pause',
+      shortcut: 'alt + space'
+    },
+    {
+      action: 'Forward',
+      shortcut: 'alt + →'
+    },
+    {
+      action: 'Backward',
+      shortcut: 'alt + ←'
+    },
+    {
+      action: 'Navigate Forward',
+      shortcut: 'tabs'
+    },
+    {
+      action: 'Navigate Backward',
+      shortcut: 'shift + tabs'
+    }
+  ]
+
+  return (
+    <Fragment>
+      Player Shortcuts
+      <EuiSpacer size="s" />
+      <EuiBasicTable
+        items={items}
+        columns={columns}
+      />
+    </Fragment>
+  )
+}
+
+export default TrainingInstructions
