dd6d95787167223da3d331c30e5e10f6582fcac0
M Rezaur Rahman <mrezaurrahman@icloud.com>
2019-06-28 09:18:37 +0200

    Refactor code

--- src/models/audioProcessing/bufferSilenceCount.js
+++ src/models/audioProcessing/bufferSilenceCount.js
@@ -1,15 +1,6 @@
 const bufferSilenceCount = (channel) => {
-  let min = 100
-  let max = -100
   let count = 0
   for (let i = 0; i < channel.length; i += 1) {
-    if (channel[i] > max) {
-      max = channel[i]
-    }
-    if (channel[i] < min) {
-      min = channel[i]
-    }
-
     if (channel[i] > -0.001 && channel[i] < 0.001) {
       count += 1
     }
