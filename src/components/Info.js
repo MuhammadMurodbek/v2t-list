3059906e480dc643a24c01e2bb33ea73bd57cd62
Rezaur Rahman <rezaur.rahman@inoviagroup.se>
2019-09-13 10:43:51 +0200

    Fix 868

--- src/components/Info.js
+++ src/components/Info.js
@@ -19,9 +19,49 @@ export default class Info extends Component {
     console.log(personnummer)
     console.log('patientName')
     console.log(patientName)
-
     this.setState({ patientId: personnummer })
     this.setState({ patientNamn: patientName })
+  }
+
+  componentDidUpdate(prevProps) {
+    const { personnummer, patientName } = this.props
+    const pnummer = prevProps.personnummer 
+    const pName = prevProps.patientName
+    if (personnummer !== pName || personnummer !== pnummer) {
+      console.log('personnummer2')
+      console.log(personnummer)
+      console.log('patientName2')
+      console.log(patientName)
+      this.setState({ patientId: personnummer })
+      this.setState({ patientNamn: patientName })
+    }
+    
+
+
+  }
+
+  loadSegments = async () => {
+    const { personnummer, patientName } = this.props
+    const queryString = `/api/v1/transcription/${transcript.external_id}`
+    const response = await axios.get(queryString)
+    const originalChapters = this.parseTranscriptions(response.data.transcriptions)
+    const { tags, fields } = response.data
+    if (tags) {
+      this.setState({ originalChapters, tags })
+    } else {
+      this.setState({ originalChapters, tags: [] })
+    }
+
+    if (fields) {
+      this.setState({ fields })
+    } else {
+      this.setState({
+        fields: {
+          patient_id: '',
+          patient_full_name: ''
+        }
+      })
+    }
   }
 
   changePersonnummmerEditStatus = () => {
