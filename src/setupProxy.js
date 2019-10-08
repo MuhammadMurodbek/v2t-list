d84f80ed7f8913b8706568930dedaad4ed340d24
Rezaur Rahman <rezaur.rahman@inoviagroup.se>
2019-10-08 09:15:34 +0200

    Add login API in proxy settings

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -8,4 +8,5 @@ module.exports = (app) => {
   app.use(proxy('/api/v1/search/icd-10', { target: 'http://v2t-word-spotter:6100/', secure: false, changeOrigin: true }))
   app.use(proxy('/api/v1/v2t-service-realtime/', { target: 'http://v2t-transcription:6100/', secure: false, changeOrigin: true }))
   app.use(proxy('/api/v1/training/', { target: 'http://v2t-transcription:6100/', secure: false, changeOrigin: true }))
+  app.use(proxy('/api/login/v1/', { target: 'http://v2t-api:6100/', secure: false, changeOrigin: true }))
 }
