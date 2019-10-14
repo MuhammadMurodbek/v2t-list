28c1817378be16e1eb242a22ac865b5d179a5cb8
Rezaur Rahman <rezaur.rahman@inoviagroup.se>
2019-10-14 14:42:47 +0200

    Add template component

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,30 +1,16 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
-import React, {Fragment} from 'react'
+import React, { Fragment } from 'react'
+import axios from 'axios'
 import {
-    EuiSpacer,
-    EuiText
+  EuiSpacer,
+  EuiText
 } from '@elastic/eui'
-import PropTypes from 'prop-types'
-
+import { usePreferences } from '../components/PreferencesProvider'
+import Page from '../components/Page'
 
 const DropDown = ({ title, content }) => (
-    <Fragment>
-        <strong>{title}</strong>
-        <EuiSpacer size="xs" />
-        <EuiText size="s" color="subdued">
-            <p className="euiTextColor--subdued">
-                {content}
-            </p>
-        </EuiText>
-    </Fragment>
+  <Fragment>
+    <strong>{title}</strong>
+  </Fragment>
 )
-
-DropDown.propTypes = {
-    title: PropTypes.string.isRequired,
-    content: PropTypes.string
-}
-
 
 export default DropDown
