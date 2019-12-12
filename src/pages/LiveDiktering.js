38f49b3464b1a451d22aaa8e89dc68a136da1524
Rezaur Rahman <rezaur.rahman@inoviagroup.se>
2019-12-13 00:08:50 +0100

    Use mediaDevices webAPI for mocrophone access

--- src/pages/LiveDiktering.js
+++ src/pages/LiveDiktering.js
@@ -21,7 +21,8 @@ export default class LiveDikterin2 extends Component {
     chapters: [{ keyword: "KONTAKTORSAK", segments: [{ words: "...", startTime: 0.00, endTime: 0.00 }] }],
     originalText: '',
     currentText: '',
-    sections: ["KONTAKTORSAK", "AT", "LUNGOR", "BUK", "DIAGNOS"]
+    sections: ["KONTAKTORSAK", "AT", "LUNGOR", "BUK", "DIAGNOS"],
+    isMicrophoneStarted: false
   }
 
   componentDidMount = () => {
@@ -166,17 +167,50 @@ export default class LiveDikterin2 extends Component {
   }
 
   initAudio = () => {
-    if (!navigator.getUserMedia)
-      navigator.getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-    if (!navigator.cancelAnimationFrame)
-      navigator.cancelAnimationFrame = navigator.webkitCancelAnimationFrame || navigator.mozCancelAnimationFrame;
-    if (!navigator.requestAnimationFrame)
-      navigator.requestAnimationFrame = navigator.webkitRequestAnimationFrame || navigator.mozRequestAnimationFrame;
+    // const { isMicrophoneStarted } = this.state
+    // // if (isMicrophoneStarted) {}
+    // if (!navigator.getUserMedia)
+    //   navigator.getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 
-    navigator.getUserMedia({ audio: true }, this.gotStream, function (e) {
-      alert('Error getting audio');
-      console.log(e);
-    });
+    // navigator.getUserMedia({ audio: true }, this.gotStream, function (e) {
+    //   alert('Error getting audio');
+    //   console.log(e);
+    // });
+    if (navigator.mediaDevices === undefined) {
+      navigator.mediaDevices = {};
+    }
+
+    // Some browsers partially implement mediaDevices. We can't just assign an object
+    // with getUserMedia as it would overwrite existing properties.
+    // Here, we will just add the getUserMedia property if it's missing.
+    if (navigator.mediaDevices.getUserMedia === undefined) {
+      navigator.mediaDevices.getUserMedia = function (constraints) {
+
+        // First get ahold of the legacy getUserMedia, if present
+        var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+        // Some browsers just don't implement it - return a rejected promise with an error
+        // to keep a consistent interface
+        if (!getUserMedia) {
+          return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+        }
+
+        // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+        return new Promise(function (resolve, reject) {
+          getUserMedia.call(navigator, constraints, resolve, reject);
+        });
+      }
+    }
+
+
+    navigator.mediaDevices.getUserMedia({ audio: true })
+      .then((stream) => {
+        this.gotStream(stream)
+      })
+      .catch(function (err) {
+        console.log(err.name + ": " + err.message);
+      });
+
   }
 
 
