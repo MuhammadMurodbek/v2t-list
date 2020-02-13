3641476007d98619100fc98846b0c3edef986fba
Rezaur Rahman <rezaur.rahman@inoviagroup.se>
2020-02-13 15:01:30 +0100

    Remove unused code

--- src/pages/Invalid.js
+++ src/pages/Invalid.js
@@ -2,7 +2,6 @@ import React from 'react'
 import {
   EuiFlexGroup, EuiFlexItem, EuiTextAlign, EuiText
 } from '@elastic/eui'
-import Page from '../components/Page'
 import '../styles/pageNotFound.css'
 
 
