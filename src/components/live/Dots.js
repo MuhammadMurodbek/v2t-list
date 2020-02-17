b6117c2989437953a96aecab2f43228a17e2bd3b
Rezaur Rahman <rezaur.rahman@inoviagroup.se>
2020-02-17 16:00:05 +0100

    Add animation

---
+++ src/components/live/Dots.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../../styles/dots.css'
+
+const Dots = () => {
+  return (
+    <div className="spinner">
+      <div className="bounce1"></div>
+      <div className="bounce2"></div>
+      <div className="bounce3"></div>
+    </div>
+  )
+}
+
+
+export default Dots
