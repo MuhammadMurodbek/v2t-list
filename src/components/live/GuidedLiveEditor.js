b00d9bf6be97371065c96fe721b7db732d00bada
Rezaur Rahman <rezaur.rahman@inoviagroup.se>
2020-04-14 10:35:08 +0200

    Fix guided live

--- src/components/live/GuidedLiveEditor.js
+++ src/components/live/GuidedLiveEditor.js
@@ -23,7 +23,7 @@ const GuidedLiveEditor = ({
   listOfTemplates,
   templatesForMenu
 }) => {
-  const [editorVisible, setEditorVisible] = useState(true)
+  const [editorVisible, setEditorVisible] = useState(false)
   const [currentStepIndex, setCurrentStepIndex] = useState(0)
   const [tags, setTags] = useState([])
   const [chapters, setChapters] = useState([{
