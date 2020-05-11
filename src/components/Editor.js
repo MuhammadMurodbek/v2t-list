7baabd9856eeba9bb7297b431f0cf3062fbbc392
Rezaur Rahman <rezaur.rahman@inoviagroup.se>
2020-05-11 09:04:59 +0200

    Work on editor convergence

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -38,20 +38,20 @@ export default class Editor extends Component {
     this.initChapters()
   }
 
-  areChaptersEqual = (o1, o2, ignorePropsArr = []) => {
-    // Deep Clone objects
-    const _obj1 = JSON.parse(JSON.stringify(o1)),
-      _obj2 = JSON.parse(JSON.stringify(o2))
-    // Remove props to ignore
-    ignorePropsArr.forEach(p => {
-      console.log(`_obj1.${p} = _obj2.${p} = "IGNORED"`)
-    })
-    // compare as strings
-    const s1 = JSON.stringify(_obj1),
-      s2 = JSON.stringify(_obj2)
-    // return [s1==s2,s1,s2];
-    return s1 === s2
-  }
+  // areChaptersEqual = (o1, o2, ignorePropsArr = []) => {
+  //   // Deep Clone objects
+  //   const _obj1 = JSON.parse(JSON.stringify(o1)),
+  //     _obj2 = JSON.parse(JSON.stringify(o2))
+  //   // Remove props to ignore
+  //   ignorePropsArr.forEach(p => {
+  //     console.log(`_obj1.${p} = _obj2.${p} = "IGNORED"`)
+  //   })
+  //   // compare as strings
+  //   const s1 = JSON.stringify(_obj1),
+  //     s2 = JSON.stringify(_obj2)
+  //   // return [s1==s2,s1,s2];
+  //   return s1 === s2
+  // }
 
   componentDidUpdate(prevProps) {
     const { initialCursor, templateId, originalChapters } = this.props
@@ -60,7 +60,10 @@ export default class Editor extends Component {
     else
       this.updateCursor()
     
-    if (this.areChaptersEqual(prevProps.originalChapters, originalChapters) === false) {
+    if (
+      JSON.parse(JSON.stringify(JSON.stringify(prevProps.originalChapters))) 
+        !== JSON.parse(JSON.stringify(JSON.stringify(originalChapters)))
+    ) {
       this.initChapters()
     }
     
