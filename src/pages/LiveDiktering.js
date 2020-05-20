644cc227f0a52ae5281c7ebcf723032d311c3a70
Rezaur Rahman <rezaur.rahman@inoviagroup.se>
2020-05-20 11:53:49 +0200

    Refactor audio input part

--- src/pages/LiveDiktering.js
+++ src/pages/LiveDiktering.js
@@ -71,6 +71,30 @@ export default class LiveDiktering extends Component {
     if (navigator.mediaDevices === undefined) {
       navigator.mediaDevices = {}
     }
+
+    // Some browsers partially implement mediaDevices. We can't just assign an object with 
+    // getUserMedia as it would overwrite existing properties. Here, we will just add the 
+    // getUserMedia property if it's missing.
+    if (navigator.mediaDevices.getUserMedia === undefined) {
+      navigator.mediaDevices.getUserMedia = function (constraints) {
+        // First get ahold of the legacy getUserMedia, if present
+        const getUserMedia
+          = navigator.webkitGetUserMedia || navigator.mozGetUserMedia
+        // Some browsers just don't implement it - return a rejected promise with an error
+        // to keep a consistent interface
+        if (!getUserMedia) {
+          return Promise
+            .reject(
+              new Error('getUserMedia is not implemented in this browser')
+            )
+        }
+
+        // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+        return new Promise(function (resolve, reject) {
+          getUserMedia.call(navigator, constraints, resolve, reject)
+        })
+      }
+    }
   }
 
   templates = async () => {
@@ -196,41 +220,10 @@ export default class LiveDiktering extends Component {
   }
 
   onCursorTimeChange = (cursorTime, chapterId, segmentId, timestampStart, timestampEnd) => {
-    // console.log('cursorTime')
-    // console.log(cursorTime)
-    // console.log('start time')
-    // console.log(timestampStart)
-    // console.log('end time')
-    // console.log(timestampEnd)
-    // console.log('cursorTime end')
     this.setState({ cursorTime, chapterId, segmentId })
   }
 
   initAudio = async () => {
-    // Some browsers partially implement mediaDevices. We can't just assign an object with 
-    // getUserMedia as it would overwrite existing properties. Here, we will just add the 
-    // getUserMedia property if it's missing.
-    if (navigator.mediaDevices.getUserMedia === undefined) {
-      navigator.mediaDevices.getUserMedia = function (constraints) {
-        // First get ahold of the legacy getUserMedia, if present
-        const getUserMedia
-          = navigator.webkitGetUserMedia || navigator.mozGetUserMedia
-        // Some browsers just don't implement it - return a rejected promise with an error
-        // to keep a consistent interface
-        if (!getUserMedia) {
-          return Promise
-            .reject(
-              new Error('getUserMedia is not implemented in this browser')
-            )
-        }
-
-        // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
-        return new Promise(function (resolve, reject) {
-          getUserMedia.call(navigator, constraints, resolve, reject)
-        })
-      }
-    }
-
     await navigator.mediaDevices.getUserMedia({ audio: true })
       .then((stream) => {
         recorder.init(stream)
