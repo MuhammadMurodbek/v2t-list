53994d8bd375cdaf07bc9f5187600e04cd43bd38
Abdulbosit Khamidov <abdulbosit.khamidov@inoviagroup.se>
2020-08-06 10:21:50 +0200

    Revert "Switching to webpack from create-react-app"
    This reverts commit 7fc49663c10b033ae404e6a95c0222bb86701d50

--- server/config.js
+++
@@ -1,7 +0,0 @@
-const baseUrl =
-  process.env.NODE_ENV === 'development'
-    ? 'https://v2t-dev.inoviagroup.se/api'
-    : ''
-
-const frontEndDevPort = 13388
-module.exports = { baseUrl, frontEndDevPort }
---
+++ src/setupProxy.js
@@ -0,0 +1,8 @@
+/* eslint-disable max-len */
+const proxy = require('http-proxy-middleware')
+
+module.exports = (app) => {
+  app.use(proxy('/api/', { target: 'http://v2t-api:6100', secure: false, changeOrigin: true }))
+  // app.use(proxy('/api/', { target: 'https://v2t-dev.inoviagroup.se/#/', secure: false, changeOrigin: true }))
+  // app.use(proxy('/api/', { target: 'http://www-v2t-dev.inoviagroup.se/#/', secure: false, changeOrigin: true }))
+}
