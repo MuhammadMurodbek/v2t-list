b242fd7a4505c91e966b1c068aa0c2125a5c88a8
Rezaur  Rahman <rezaur.rahman@inoviagroup.se>
2020-08-07 17:02:28 +0200

    Print sample rate!

--- src/pages/LiveDiktering.js
+++ src/pages/LiveDiktering.js
@@ -273,6 +273,7 @@ export default class LiveDiktering extends Component {
       if (recording === true) {
         let input = audioEvent.inputBuffer.getChannelData(0)
         input = interpolateArray(input, 16000, prevState.audioContext.sampleRate)
+        console.log(`sample rate :: ${prevState.audioContext.sampleRate}`)        
         // convert float audio data to 16-bit PCM
         var buffer = new ArrayBuffer(input.length * 2)
         var output = new DataView(buffer)
